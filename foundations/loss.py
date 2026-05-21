933e2002cc25caa317c5ef9d051ade401c53711e
Mihai Tira <68811250+waytoounoriginal@users.noreply.github.com>
2026-05-21 19:06:57 +0300

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -9,12 +9,10 @@ class Solution:
         # y_pred: predicted probabilities
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
-        n = y_true.shape[0]
-
         true_low = y_true * np.log(y_pred)
         false_high = (1 - y_true) * np.log(1 - y_pred)
 
-        ans = -1/n * np.sum(true_low + false_high)
+        ans = -np.mean(true_low + false_high)
         return round(ans, 4)
 
     def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
@@ -22,7 +20,5 @@ class Solution:
         # y_pred: predicted probabilities (shape: n_samples x n_classes)
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
-        n = y_true.shape[0]
-
-        ans = -1/n * np.sum(y_true * np.log(y_pred))
+        ans = -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
         return round(ans, 4)
